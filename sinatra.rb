9357df22d9fd63e31b3d36c8273fc82681355b25
Oren Kanner <okanner@gmail.com>
2014-03-09 23:04:18 -0400

    set up #create and #read and some tests, among other things

--- sinatra.rb
+++ sinatra.rb
@@ -7,6 +7,14 @@ configure do
 	enable :method_override
 end
 
+helpers do
+	# check for set existence
+	def set_exists?(set)
+		@sets = session[:sets]
+		!@sets[set]
+	end
+end
+
 # home page
 get '/' do
 	erb :application do
@@ -22,8 +30,57 @@ get '/sets/?' do
 	end
 end
 
-get '/lists' do
+# create and view sets with the same path?
+get '/sets/:name/?' do
+	@name = params[:name] == "new" ? "" : params[:name]
+	@sets = session[:sets]
+	@set = @sets ? @sets[@name] : nil
 	erb :application do
-		erb :lists
+		if @set
+			erb :show
+		else
+			erb :new
+		end
 	end
+end
+
+# create a set
+post '/sets/new' do
+	@name = params[:name]
+	@urls = params[:urls].split(',').map(&:strip)
+	if @name.empty? or @urls == []
+		@error = 'invalid parameters'
+		erb :application do
+			erb :new
+		end
+	else
+		@sets = session[:sets] ? session[:sets] : {}
+		@sets[@name] = { name: @name, urls: @urls }
+		session[:sets] = @sets
+
+		erb :application do
+			erb :index
+		end
+	end
+
+	# redirect to("/sets/#{@name}")
+	# NOTE: we can't use redirects if we want to test the responses to requests
+end
+
+get '*/params/?' do
+	erb :application do
+		params.inspect
+	end
+end
+
+get '/session/?' do
+	erb :application do
+		session.inspect
+	end
+end
+
+get '/session/clear' do
+	session.clear
+
+	redirect to('/session')
 end
--- spec/features/page_spec.rb
+++ spec/features/page_spec.rb
@@ -13,4 +13,38 @@ describe 'site pages' do
 		before { visit '/' }
 		it_behaves_like 'all pages'
 	end
+
+	describe 'Sets page', type: :feature do
+		before { visit '/sets' }
+		it_behaves_like 'all pages'
+		it { should have_selector('h1', text: 'Sets') }
+
+		context 'with existing sets' do
+			before { post '/sets/new', params={ name: 'pants', urls: 'a, b,c' } }
+			it 'should show the set' do
+				visit '/sets'
+				last_response.body.should have_content 'pants'
+			end
+		end
+	end
+
+	describe 'New set page', type: :feature do
+		before { visit '/sets/new' }
+		context 'with no set name' do
+			before { fill_in 'urls', with: 'foobar' }
+			it 'should not save' do
+				click_button 'submit'
+				expect(page).to have_no_content('foobar')
+			end
+		end
+
+		context 'with no links' do
+			before { fill_in 'name', with: 'buzzbar' }
+			it 'should not save' do
+				click_button 'submit'
+				expect(page).to have_no_content('buzzbar')
+			end
+		end
+	end
+
 end
