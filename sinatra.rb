c4470163f0fb85cd6fa5dc4d3c1c26f91255cb05
Oren Kanner <okanner@gmail.com>
2014-03-13 23:27:30 -0400

    added play helpers

--- sinatra.rb
+++ sinatra.rb
@@ -36,6 +36,18 @@ helpers do
 			session[:sets] = @sets
 		end
 	end
+
+	def randomvideo(set)
+		set.sample
+	end
+
+	def embedyoutube(videonumber)
+		%{
+		<body style="margin:0;">
+		<object height="100%" width="100%"><param name="movie" value="http://www.youtube.com/v/#{videonumber}&autoplay=1" /><embed height="100%" src="http://www.youtube.com/v/#{videonumber}&autoplay=1" type="application/x-shockwave-flash" width="100%"></embed></object>
+		</body>
+		}
+	end
 end
 
 # home page
